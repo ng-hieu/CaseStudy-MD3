c94c8ffe4cefe294cdab3afc46bb5933521cdf0f
quocm <quocminhhoang5004@gmail.com>
2023-04-05 10:35:48 +0700

    Minh làm productController và service

---
+++ service/productService.js
@@ -0,0 +1,22 @@
+const connection = require ('../entity/connection.js')
+
+class ProductService {
+    connect;
+    constructor() {
+        connection.connectingToMySQL();
+        this.connect = connection.getConnection();
+    }
+    showAll = () => {
+        return new Promise((resolve, reject) => {
+            this.connect.query('select productId, nameProduct, priceProduct, quantityProduct from product_list', (error, data) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(data);
+                }
+            })
+        })
+    }
+}
+
+module.exports = new ProductService();
