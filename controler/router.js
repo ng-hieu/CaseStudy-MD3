583e46a562ce9db0d1dac577bf09a2c179fd373d
phong0710 <phonglh071099@gmail.com>
2023-04-07 18:21:43 +0700

    Phong them gio hang

--- controler/router.js
+++ controler/router.js
@@ -1,15 +1,17 @@
 const productController = require('./handle/productController');
-const  userController= require('./handle/userController');
+const userController = require('./handle/userController');
 const router = {
     "home": productController.home,
     'edit': userController.editProductById,
     'homeAdmin': userController.showProductInAdmin,
-    'descriptionProduct':productController.descriptionProduct,
+    'descriptionProduct': productController.descriptionProduct,
     'signin': userController.signIn,
     'signup': userController.signUp,
     'addProduct': userController.addProduct,
-    "":productController.homeBfsign,
-    "signout":productController.homeBfsign
+    "": productController.homeBfsign,
+    "signout": productController.homeBfsign,
+    "shoppingCart": productController.showShoppingCart,
+    'addToCart':productController.addShoppingCart
 }
 
 module.exports = router;
